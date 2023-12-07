5c28f55bf2f54e39e8573e1305f07da94df8bb3a
Arthur <146384817+ArthurGianichini@users.noreply.github.com>
2023-12-07 17:38:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,170 @@
+const tela = document.querySelectorAll('.Tela')
+const palavras = document.querySelectorAll(".Palavra")
+const fechar = document.querySelectorAll(".fechar")
+const audioEn = document.querySelectorAll('.audioEn')
+const audioPtBr = document.querySelectorAll('.audioPtBr')
+
+
+palavras[0].addEventListener('click', ()=>{
+    tela[0].style.display = 'block'
+})
+fechar[0].addEventListener('click', ()=>{
+    tela[0].style.display = 'none'
+})
+audioEn[0].addEventListener('click', ()=>{
+    const audio = new Audio('./audio/Effort.mp3');
+        audio.play();
+} )
+audioPtBr[0].addEventListener('click', ()=>{
+    const audio = new Audio('./audio/Esforço.mp3');
+        audio.play();
+} )
+
+
+
+palavras[1].addEventListener('click', ()=>{
+    tela[1].style.display = 'block'
+})
+fechar[1].addEventListener('click', ()=>{
+    tela[1].style.display = 'none'
+})
+audioEn[1].addEventListener('click', ()=>{
+    const audio = new Audio('./audio/Literraly.mp3');
+        audio.play();
+})
+audioPtBr[1].addEventListener('click', ()=>{
+    const audio = new Audio('./audio/Literalmente.mp3');
+        audio.play();
+})
+
+
+
+palavras[2].addEventListener('click', ()=>{
+    tela[2].style.display = 'block'
+})
+fechar[2].addEventListener('click', ()=>{
+    tela[2].style.display = 'none'
+})
+audioEn[2].addEventListener('click', ()=>{
+    const audio = new Audio('./audio/Hesitant.mp3');
+        audio.play();
+})
+audioPtBr[2].addEventListener('click', ()=>{
+    const audio = new Audio('./audio/Hesitante.mp3');
+        audio.play();
+})
+
+
+
+palavras[3].addEventListener('click', ()=>{
+    tela[3].style.display = 'block'
+})
+fechar[3].addEventListener('click', ()=>{
+    tela[3].style.display = 'none'
+})
+audioEn[3].addEventListener('click', ()=>{
+    const audio = new Audio('./audio/Dependable.mp3');
+        audio.play();
+})
+audioPtBr[3].addEventListener('click', ()=>{
+    const audio = new Audio('./audio/Confiável.mp3');
+        audio.play();
+})
+
+
+palavras[4].addEventListener('click', ()=>{
+    tela[4].style.display = 'block'
+})
+fechar[4].addEventListener('click', ()=>{
+    tela[4].style.display = 'none'
+})
+audioEn[4].addEventListener('click', ()=>{
+    const audio = new Audio('./audio/Where.mp3');
+        audio.play();
+})
+audioPtBr[4].addEventListener('click', ()=>{
+    const audio = new Audio('./audio/Onde.mp3');
+        audio.play();
+})
+
+
+palavras[5].addEventListener('click', ()=>{
+    tela[5].style.display = 'block'
+})
+fechar[5].addEventListener('click', ()=>{
+    tela[5].style.display = 'none'
+})
+audioEn[5].addEventListener('click', ()=>{
+    const audio = new Audio('./audio/Were.mp3');
+        audio.play();
+})
+audioPtBr[5].addEventListener('click', ()=>{
+    const audio = new Audio('./audio/Eram.mp3');
+        audio.play();
+})
+
+
+
+palavras[6].addEventListener('click', ()=>{
+    tela[6].style.display = 'block'
+})
+fechar[6].addEventListener('click', ()=>{
+    tela[6].style.display = 'none'
+})
+audioEn[6].addEventListener('click', ()=>{
+    const audio = new Audio('./audio/Successful.mp3');
+        audio.play();
+})
+audioPtBr[6].addEventListener('click', ()=>{
+    const audio = new Audio('./audio/Bem-sucedido.mp3');
+        audio.play();
+})
+
+
+palavras[7].addEventListener('click', ()=>{
+    tela[7].style.display = 'block'
+})
+fechar[7].addEventListener('click', ()=>{
+    tela[7].style.display = 'none'
+})
+audioEn[7].addEventListener('click', ()=>{
+    const audio = new Audio('./audio/Responsible.mp3');
+        audio.play();
+})
+audioPtBr[7].addEventListener('click', ()=>{
+    const audio = new Audio('./audio/Responsável.mp3');
+        audio.play();
+})
+
+
+palavras[8].addEventListener('click', ()=>{
+    tela[8].style.display = 'block'
+})
+fechar[8].addEventListener('click', ()=>{
+    tela[8].style.display = 'none'
+})
+audioEn[8].addEventListener('click', ()=>{
+    const audio = new Audio('./audio/Motivated.mp3');
+        audio.play();
+})
+audioPtBr[8].addEventListener('click', ()=>{
+    const audio = new Audio('./audio/Motivado.mp3');
+        audio.play();
+})
+
+
+
+palavras[9].addEventListener('click', ()=>{
+    tela[9].style.display = 'block'
+})
+fechar[9].addEventListener('click', ()=>{
+    tela[9].style.display = 'none'
+})
+audioEn[9].addEventListener('click', ()=>{
+    const audio = new Audio('./audio/Curiosity.mp3');
+        audio.play();
+})
+audioPtBr[9].addEventListener('click', ()=>{
+    const audio = new Audio('./audio/Curiosidade.mp3');
+        audio.play();
+})
